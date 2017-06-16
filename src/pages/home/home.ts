2ea488f31cefdf212f8d3219b2e8538d5a8458d4
Is2 <haijiandong@outlook.com>
2017-06-16 17:19:05 +0800

    修改tab，增加HTTP

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
+import {Http} from '.4.1.2@@angular/http';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  posts: any;
+  constructor(public navCtrl: NavController,public http:Http) {
+     this.http.get("http://weixin.sogou.com/wapindex/wap/0612/wap_19/0.html").map(res => res.toString()).subscribe(data => {
+        this.posts = data;
+    });
+ 
   }
 
 }
